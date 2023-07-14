67853f7a39d0e2790f58c355fc0fb3e8e61c1e76
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-07-14 16:51:01 -0300

    limpeza

--- bot-fy/Discord/Events.cs
+++ bot-fy/Discord/Events.cs
@@ -1,5 +1,4 @@
 ﻿using DSharpPlus;
-using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using Serilog;
 
@@ -10,16 +9,12 @@ namespace bot_fy.Discord
         public static Task OnSessionCreated(DiscordClient client, SessionReadyEventArgs e)
         {
             Log.Information($"Bot conectado como {client.CurrentUser.Username}#{client.CurrentUser.Discriminator}");
-            //client.UpdateCurrentUserAsync(client.CurrentUser.Username, client.CurrentUser.GetAvatarAsync().Result);
             return Task.CompletedTask;
         }
 
         public static Task OnGuildDownloadCompleted(DiscordClient client, GuildDownloadCompletedEventArgs e)
         {
             Log.Information($"Carregado {e.Guilds.Count} servidores");
-            foreach (DiscordGuild guild in e.Guilds.Values)
-            {
-            }
             return Task.CompletedTask;
         }
     }
