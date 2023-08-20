ebd0c8479beb632c7b56aacc12baeafcec9be43d
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-08-20 20:00:34 -0300

    Otimizado fila de videos

--- bot-fy/Discord/Events.cs
+++ bot-fy/Discord/Events.cs
@@ -1,21 +1,53 @@
-﻿using DSharpPlus;
+﻿using bot_fy.Commands;
+using bot_fy.Extensions.Discord;
+using DSharpPlus;
 using DSharpPlus.EventArgs;
 using Serilog;
+using YoutubeExplode.Videos;
 
 namespace bot_fy.Discord
 {
     public class Events
     {
-        public static Task OnSessionCreated(DiscordClient client, SessionReadyEventArgs e)
+        public static Task OnSessionCreated(DiscordClient client, SessionReadyEventArgs args)
         {
             Log.Information($"Bot conectado como {client.CurrentUser.Username}#{client.CurrentUser.Discriminator}");
             return Task.CompletedTask;
         }
 
-        public static Task OnGuildDownloadCompleted(DiscordClient client, GuildDownloadCompletedEventArgs e)
+        public static Task OnGuildDownloadCompleted(DiscordClient client, GuildDownloadCompletedEventArgs args)
         {
-            Log.Information($"Carregado {e.Guilds.Count} servidores");
+            Log.Information($"Carregado {args.Guilds.Count} servidores");
+            Log.Information($"Carregado {args.Guilds.Sum(g => g.Value.MemberCount)} membros");
+            Log.Information($"Carregado {args.Guilds.Sum(g => g.Value.Channels.Count)} canais");
             return Task.CompletedTask;
+        }
+
+        public static async Task OnComponentInteractionCreated(DiscordClient client, ComponentInteractionCreateEventArgs args)
+        {
+            await args.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
+
+            if (args.Id == "skip")
+            {
+                MusicCommand.Skip(args.Guild.Id);
+            }
+
+            else if (args.Id == "stop")
+            {
+                MusicCommand.Stop(args.Guild.Id);
+            }
+
+            else if (args.Id == "shuffle")
+            {
+                MusicCommand.Shuffle(args.Guild.Id);
+            }
+
+            else if (args.Id == "queue")
+            {
+                IEnumerable<IVideo> queue = MusicCommand.GetQueue(args.Guild.Id);
+
+                await args.Channel.SendPaginatedMusicsAsync(args.User, queue);
+            }
         }
     }
 }
